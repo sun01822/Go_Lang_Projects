bd25d57555c256a2a002d3bb4f753322a353c159
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-12-31 00:42:02 +0600

    MySQL connection and data insertion

--- connecting_mySql/main.go
+++ connecting_mySql/main.go
@@ -6,7 +6,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main(){
+func main() {
 	fmt.Println("Go MySQL Tutorial")
 
 	// Open up our database connection.
@@ -15,14 +15,25 @@ func main(){
 	db, err := sql.Open("mysql", "sun01822:@Sun2021Yes#@tcp(127.0.0.1:3306)/students")
 
 	// if there is an error opening the connection, handle it
-	if err != nil{
+	if err != nil {
 		panic(err.Error())
-	}else{
+	} else {
 		fmt.Println("Database connected successfully")
 	}
-
 
 	// defer the close till after the main function has finished
 	defer db.Close()
 
+	// perform a db.Query insert
+	insert, err := db.Query("INSERT INTO students VALUES ( 4, 'Saikot', 'EEE'),( 5, 'Sakib', 'CSE'),( 6, 'Sakil', 'BBA');")
+	
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("Data inserted successfully")
+	}
+
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
 }
